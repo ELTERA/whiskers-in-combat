040934feb46f7b5a012b8eee159f8691b598f1e3
EL <EL@DEV.NUL>
2020-12-13 12:16:02 +0100

    Keep it simple

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-String.prototype.clr = function (hexColor) { return `<font color='#${hexColor}'>${this}</font>` };
 
 module.exports = function WhiskersInCombat(mod) {    
     const WhiskerIDs = [206100, 206101, 206102, 206103, 206104, 206105, 206106, 206107, 206108, 206109];
@@ -10,13 +8,11 @@ module.exports = function WhiskersInCombat(mod) {
     mod.game.me.on('enter_combat', () => { 
         if (mod.game.inventory.findInEquipment(WhiskerIDs)) {
             if (mod.settings.proxyMessage) 
-                mod.command.message(Message.clr("FF0000"));
-            
-            if (mod.settings.whisperMessage)
-                mod.send('S_CHAT', 2, {
-                    channel: 7, 
-                    authorName: '',
-                    message: Message
+                mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
+                    "type": 44,
+                    "chat": false,
+                    "channel": 0,
+                    "message": Message
                 });
         }
     })
