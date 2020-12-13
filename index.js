a4e42f48e65af4d75113847dae50202189fd7ccf
EL <EL@DEV.NUL>
2020-12-13 13:40:45 +0100

    more precise message

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
 module.exports = function WhiskersInCombat(mod) {    
     const WhiskerIDs = [206100, 206101, 206102, 206103, 206104, 206105, 206106, 206107, 206108, 206109];
-    const Message = 'Angler Whiskers are equipped!';
+    const Message = ' Angler Whiskers are equipped !';
     
     mod.game.initialize('inventory');
     
-    mod.game.me.on('enter_combat', () => { 
+    mod.game.me.on('change_zone', () => { 
         if (mod.game.inventory.findInEquipment(WhiskerIDs)) {
-                mod.send('S_DUNGEON_EVENT_MESSAGE', 2, {
-                "type": 44,
-                "chat": false,
-                "channel": 0,
-                "message": Message
+            if (mod.game.me.inDungeon)
+                mod.send('S_CHAT', 3, {
+                    channel: 21,
+                    gm: 1,
+                    name: 'Dungeon-Alert',
+                    message: Message                    
             });
         }
     })
